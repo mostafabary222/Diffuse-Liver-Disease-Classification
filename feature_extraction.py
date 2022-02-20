c92b46328ab4c3c7112cec0ab736f3475b0fcf1a
Ahmed <memorykart1@gmail.com>
2022-02-20 02:58:19 +0200

    ultrasound classifcation

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,15 +1,22 @@
-import cv2 as cv
+# import cv2 as cv
+import SimpleITK as sitk
 import numpy as np
 import os
 from skimage.feature.texture import greycomatrix
 from skimage.feature.texture import greycoprops
 from skimage.measure import shannon_entropy
-import pyfeats
+from radiomics import glrlm, glcm, firstorder
+# import pyfeats
 import pandas as pd
 import math
 
 
-def read_images(folder = "dataset/train", classes = ["normal", "fatty"]):
+def read_images(folder = "dataset/train",
+                classes = [
+                            "normal",
+                            "fatty",
+                            "cirrhosis"
+                        ]):
     image_names = {}
     images = []
     # Get all image names in folders
@@ -19,29 +26,27 @@ def read_images(folder = "dataset/train", classes = ["normal", "fatty"]):
     # read all images to list
     for cls in classes:
         for name in image_names[cls]:
-            img = cv.imread(f'{folder}/{cls}/{name}', cv.IMREAD_GRAYSCALE)
-            images.append((img,cls))
+            mask = []
+            with open(f'dataset/masks/{name[0:-4]}.txt', 'r') as file:
+                data = file.read()
+                data = data.strip().split('\n')
+                for line in data:
+                    x, y = line.split(',')
+                    mask.append((int(y),int(x)))
+            img = sitk.ReadImage(f'{folder}/{cls}/{name}', sitk.sitkUInt8)
+            images.append((img,cls,mask))
     return images
 
 
 def extract_roi(img, start , size = (32,32)):
+    img = sitk.GetArrayFromImage(img)
     roi = img[start[0]:start[0]+size[0],start[1]:start[1]+size[1]]
     mask = np.zeros(img.shape)
     mask[start[0]:start[0]+size[0],start[1]:start[1]+size[1]] = 1
     return roi, mask
 
 
-def feature_extraction(img, roi_pos=[
-    (160, 230),
-    (118, 224),
-    (241, 151),
-    (120, 420),
-    (170, 300),
-    (400, 200),
-    (300, 120),
-    (240, 240),
-    (360, 160)
-]):
+def feature_extraction(img, roi_pos):
     roi_mask_arr = []
     for pos in roi_pos:
         roi_mask_arr.append(extract_roi(img, pos))
@@ -69,11 +74,9 @@ def feature_extraction(img, roi_pos=[
 
     feat_arr = []
     for roi, mask in roi_mask_arr:
-        if roi.shape != (32, 32):
-            continue
         features = {}
 
-        glcm_mtx = greycomatrix(roi, distances=[1, 2, 3], angles=angles, levels=256)
+        glcm_mtx = greycomatrix(roi, distances = [1,2,3], angles = angles, levels = 256)
         con = greycoprops(glcm_mtx, 'contrast').flatten()
         hom = greycoprops(glcm_mtx, 'homogeneity').flatten()
         en = greycoprops(glcm_mtx, 'energy').flatten()
@@ -86,15 +89,35 @@ def feature_extraction(img, roi_pos=[
             features[f'correlation_{da_dict[j]}'] = corr[j]
 
         features[f'entropy'] = shannon_entropy(roi)
-        # features[f'var'] = np.var(roi)
-        # features[f'mean'] = np.mean(roi)
-        # GLRLM Features
-        #         feat, labels = pyfeats.glrlm_features(img, mask, 256)
-        #         for i in range(len(labels)):
-        #             features[labels[i]] = feat[i]
-        #         glrlm = {l : f for l,f in zip(labels,feat)}
-        #         features[f'longRunEmphasis'] = glrlm['GLRLM_LongRunEmphasis']
-        #         features[f'runPercentage'] = glrlm['GLRLM_RunPercentage']
+
+        features[f'mean'] = np.mean(roi)
+        features[f'variance'] = np.var(roi)
+
+        # pyradiomics
+        # mask = sitk.GetImageFromArray(mask)
+        # # First Order features
+        # firstOrderFeatures = firstorder.RadiomicsFirstOrder(img, mask)
+        # # firstOrderFeatures.enableFeatureByName('Mean', True)
+        # firstOrderFeatures.enableAllFeatures()
+        # results = firstOrderFeatures.execute()
+        # for col in results.keys():
+        #     features[col] = results[col].item()
+
+        # GLCM features
+        # glcmFeatures = glcm.RadiomicsGLCM(img, mask)
+        # glcmFeatures.enableAllFeatures()
+        # results = glcmFeatures.execute()
+        # for col in results.keys():
+        #     features[col] = results[col].item()
+        #
+        # GLRLM features
+        # glrlmFeatures = glrlm.RadiomicsGLRLM(img, mask)
+        # glrlmFeatures.enableAllFeatures()
+        # results = glrlmFeatures.execute()
+        # features['LongRunEmphasis'] = results['LongRunEmphasis'].item()
+        # features['RunPercentage'] = results['RunPercentage'].item()
+        # for col in results.keys():
+        #     features[col] = results[col].item()
 
         feat_arr.append(features)
 
@@ -115,9 +138,12 @@ def build_dataframe(images):
     # column name roiNum_feature
     data = pd.DataFrame()
 
-    for img, cls, mask in images:
+    for name, img, cls, mask in images:
         feat_arr = feature_extraction(img, mask)
+        count = 1
         for row in feat_arr:
+            row['name'] = name
+            count += 1
             row['target'] = cls
             data = data.append(row,ignore_index=True)
     return data
